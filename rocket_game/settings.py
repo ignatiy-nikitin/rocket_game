ed9bdaccdfec82b58e1bbf6051a1311393ebcb34
ignatiy-nikitin <ignat.no@yandex.ru>
2021-10-04 16:57:52 +0300

    f4

--- rocket_game/settings.py
+++ rocket_game/settings.py
@@ -127,7 +127,7 @@ import os
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
